c3b5b55bab35b68731558a4ecde02ab26a290418
Jonathan Abrantes <54941941+jonathanabrantes@users.noreply.github.com>
2022-09-05 10:33:34 -0300

    Update installdnf.sh

--- installdnf.sh
+++ installdnf.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#instala a aplicação wget
-yum install wget -y
-#baixar o DNF e suas dependencias
-wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64//dnf-0.6.4-2.sdl7.noarch.rpm
-wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/dnf-conf-0.6.4-2.sdl7.noarch.rpm
-wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/python-dnf-0.6.4-2.sdl7.noarch.rpm
-#Instalar o DNF e suas dependencias
-yum install dnf-0.6.4-2.sdl7.noarch.rpm dnf-conf-0.6.4-2.sdl7.noarch.rpm python-dnf-0.6.4-2.sdl7.noarch.rpm --skip-broken
+#atualiza pacotes e instala a aplicação dnf e wget
+yum update -y && yum install dnf wget -y
